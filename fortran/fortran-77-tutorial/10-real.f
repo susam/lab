9ac20e3a9be9db3e8de42e6c6c3f2b9856483339
Susam Pal <susam@susam.in>
2019-06-05 15:48:28 +0530

    Fortran 77: Examples of non-standard declarations

--- fortran/fortran-77-tutorial/10-real.f
+++ fortran/fortran-77-tutorial/10-real.f
@@ -4,16 +4,34 @@
       double precision b
 
 c
-c Error: GNU Extension: Nonstandard type declaration REAL*8 at (1)
+c Warning: GNU Extension: Nonstandard type declaration REAL*4 at (1)
 c Warning: GNU Extension: Nonstandard type declaration REAL*8 at (1) 
-c
-      real*8 c
+c Warning: GNU Extension: Nonstandard type declaration REAL*16 at (1)
 
-      a = 10
+      real*4 c
+      real*8 d
+      real*16 e
+
+c Error: Old-style type declaration REAL*1 not supported at (1)
+c Error: Old-style type declaration REAL*2 not supported at (1)
+c Error: Old-style type declaration REAL*3 not supported at (1)
+c Error: Old-style type declaration REAL*12 not supported at (1)
+
+c     real*1 f
+c     real*2 f
+c     real*3 f
+c     real*12 f
+
+c Error: Unclassifiable statement at (1)
+c     double f
+
+      a = 10.0
       b = 20.0
-      c = 30.0
+      c = 10.0
+      d = 20.0
+      e = 30.0
 
-      write (*,*) a, b, c
+      write (*,*) a, b, c, d, e
 
       stop
       end
