576cf0565e1ce2463a17ae150aab2c3f89d72637
Susam Pal <susam@susam.net>
2023-08-15 01:15:15 +0100

    Add guessing game

--- rust/book/p003-game/src/main.rs
+++ rust/book/p003-game/src/main.rs
@@ -3,23 +3,32 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let mut guess = String::new();
-    println!("The variable guess has been initialized to: \"{guess}\"");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
+    println!("The secret number is {secret_number}.");
 
-    println!("Guess the number!");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess_str: u32 = guess.trim().parse().expect("Please type a number");
+    loop {
+        let mut guess = String::new();
+        println!("The variable guess has been initialized to: \"{guess}\"");
 
-    println!("You guessed: {guess}");
+        println!("Guess the number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("The guessed number is [{guess}].");
 
-    match guess_str.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed {guess}.");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
