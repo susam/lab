4225b2b756b06f7c4d90ab84f2580b67a296ee3a
Susam Pal <susam@susam.net>
2024-11-07 09:32:42 +0000

    Add program to search for SHA-256 prefixes

---
+++ crypto/sha-256-prefix-match/shapre.rs
@@ -0,0 +1,80 @@
+use sha2::{Digest, Sha256};
+use std::any::type_name_of_val;
+use std::time::Instant;
+
+macro_rules! cast {
+    ($value: expr, $target_type: ty) => {
+        <$target_type>::try_from($value).unwrap()
+    };
+}
+
+fn _printt<T>(value: &T) {
+    println!("{}", type_name_of_val(value));
+}
+
+fn check(candidate: String) -> bool {
+    let msg = "The SHA-256 digest of this message, in hexadecimal form, begins with the digits "
+        .to_owned()
+        + &candidate
+        + ".";
+    let digest = Sha256::digest(&msg);
+    let result = format!("{digest:x}");
+    if result.starts_with(&candidate) {
+        println!("{msg}\n{result}");
+        true
+    } else {
+        false
+    }
+}
+
+fn solve(length: u8) {
+    // Constraint: length in [0, 255].
+    let chars: [char; 16] = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+    ];
+    let base: u8 = cast!(chars.len(), u8); // Typically in [1, 32].
+    let mut arrangement: Vec<u8> = vec![0; cast!(length, usize)];
+    let max_count: u128 = cast!(base, u128).pow(cast!(length, u32)); // Typically <= 16^32 = 2^128.
+    let mut count: u128 = 0;
+    let start_time: Instant = Instant::now();
+    let modulus = 10_000_000;
+    println!("solving for length {length} with {max_count} arrangements");
+    loop {
+        let mut i: usize = 0;
+        while i == 0 || (arrangement[i - 1] == 0 && i < cast!(length, usize)) {
+            arrangement[i] = (arrangement[i] + 1) % base;
+            i += 1;
+        }
+        if i == cast!(length, usize) && arrangement[i - 1] == 0 {
+            break;
+        }
+        let candidate: String = arrangement
+            .iter()
+            .map(|&i| chars[cast!(i, usize)])
+            .collect();
+        count += 1;
+        if count == 1 || count % modulus == 0 {
+            let elapsed: u64 = start_time.elapsed().as_secs();
+            println!(
+                "[{elapsed} s] checked {} of {}",
+                count / modulus,
+                max_count / modulus,
+            )
+        }
+        if check(candidate.clone()) {
+            break;
+        }
+    }
+    println!();
+}
+
+fn main() {
+    solve(1);
+    solve(2);
+    solve(3);
+    solve(4);
+    solve(5);
+    solve(6);
+    solve(7);
+    solve(8);
+}
