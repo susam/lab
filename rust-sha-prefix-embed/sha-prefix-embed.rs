6848a4eac213333be7643a9f5cafd341a94854dd
Susam Pal <susam@susam.net>
2024-11-12 23:53:53 +0000

    Check candidates in ascending order

--- rust-sha-prefix-embed/sha-prefix-embed.rs
+++ rust-sha-prefix-embed/sha-prefix-embed.rs
@@ -7,12 +7,12 @@ macro_rules! cast {
     };
 }
 
-fn check(candidate: String) -> bool {
+fn check(candidate: &String) -> bool {
     let msg = "The SHA-256 hash of this sentence begins with ".to_owned() + &candidate + ".";
     let digest = Sha256::digest(&msg);
     let result = format!("{digest:x}");
-    if result.starts_with(&candidate) {
-        println!("{msg}\n{result}");
+    if result.starts_with(candidate) {
+        println!("\n{msg}\n{result}\n");
         true
     } else {
         false
@@ -28,8 +28,9 @@ fn solve(length: u8) {
     let mut arrangement: Vec<u8> = vec![0; cast!(length, usize)];
     let max_count: u128 = cast!(base, u128).pow(cast!(length, u32)); // Then max_count <= 16^32 = 2^128.
     let mut count: u128 = 0;
-    let chunk_size: u128 = 10_000_000;
+    let log_interval: u128 = 10_000_000;
     let start_time: Instant = Instant::now();
+
     println!("solving for length {length} with {max_count} arrangements");
     loop {
         // Generate candidate message.
@@ -39,35 +40,34 @@ fn solve(length: u8) {
             .collect();
 
         // Check if the candidate string is a solution.
-        if check(candidate.clone()) {
+        if check(&candidate) {
             break;
         }
 
-        // Compute the next arrangement for generating the next candidate.
-        let mut i: usize = 0;
-        while i == 0 || (arrangement[i - 1] == 0 && i < cast!(length, usize)) {
-            arrangement[i] = (arrangement[i] + 1) % base;
-            i += 1;
-        }
-
-        // Log progress after every chunk_size iterations.
+        // Log progress after every log_interval iterations.
         count += 1;
-        if count % chunk_size == 0 || count == max_count {
+        if count % log_interval == 0 || count == max_count {
             let elapsed: u64 = start_time.elapsed().as_secs();
             let remaining: u64 = cast!((max_count - count) * cast!(elapsed, u128) / count, u64);
             println!(
-                "[{elapsed} s of {remaining} s] checked chunk {} of {}",
-                (count + chunk_size - 1) / chunk_size,
-                (max_count + chunk_size - 1) / chunk_size,
+                "[{elapsed} s of {remaining} s] checked chunk {} of {} ({candidate})",
+                (count + log_interval - 1) / log_interval,
+                (max_count + log_interval - 1) / log_interval,
             );
         }
 
+        // Compute the next arrangement for generating the next candidate.
+        let mut i: usize = cast!(length, usize); // Index of last cell incremented.
+        while i == cast!(length, usize) || (arrangement[i] == 0 && i >= 1) {
+            i -= 1;
+            arrangement[i] = (arrangement[i] + 1) % base;
+        }
+
         // Quit if there are no more arrangements to check.
-        if i == cast!(length, usize) && arrangement[i - 1] == 0 {
+        if i == 0 && arrangement[0] == 0 {
             break;
         }
     }
-    println!();
 }
 
 fn main() {
